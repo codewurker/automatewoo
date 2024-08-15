8be4be13060005731636fdeda036d25526243c41
Woo <info@woo.com>
2024-08-15 10:18:21 +0000

    Updates to 6.0.32

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/automatewoo',
-        'pretty_version' => 'dev-release/6.0.31',
-        'version' => 'dev-release/6.0.31',
-        'reference' => '3197bfafc261bf7a043c22c8c3028f560abfff12',
+        'pretty_version' => 'dev-release/6.0.32',
+        'version' => 'dev-release/6.0.32',
+        'reference' => 'e165cf236d51f3c4e5ccc8e73141b5863e45b6c1',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/automatewoo' => array(
-            'pretty_version' => 'dev-release/6.0.31',
-            'version' => 'dev-release/6.0.31',
-            'reference' => '3197bfafc261bf7a043c22c8c3028f560abfff12',
+            'pretty_version' => 'dev-release/6.0.32',
+            'version' => 'dev-release/6.0.32',
+            'reference' => 'e165cf236d51f3c4e5ccc8e73141b5863e45b6c1',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
